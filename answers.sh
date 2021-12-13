a54b3c27628b400946245b66ed11b6ca6e79b2d0
Obaida <o.alnakdali@gmail.com>
2021-12-13 22:48:21 +0200

    Count & Filter

--- answers.sh
+++ answers.sh
@@ -63,3 +63,54 @@ WHERE name like "Layal";
 ##########################
 ##  Joins
 ##########################
+
+#1
+SELECT employees.Name, companies.Name, companies.Date
+FROM employees
+INNER JOIN companies ON employees.Company = companies.Name
+
+#2
+SELECT employees.Name
+FROM employees
+INNER JOIN companies ON employees.Company = companies.Name
+WHERE companies.Date < 2000
+
+#3
+SELECT companies.Name
+FROM companies
+INNER JOIN employees ON employees.Company = companies.Name
+WHERE employees.Role = "Graphic Designer"
+
+
+##########################
+##  Count & Filter
+##########################
+
+
+#1
+SELECT * , max(Points)
+FROM students
+
+#2
+SELECT avg(Points)
+FROM students
+
+#3
+SELECT count(ID)
+FROM students
+WHERE Points = 500
+
+#4
+SELECT Name
+FROM students
+WHERE Name like "%s%"
+
+#5
+SELECT *
+FROM students
+ORDER BY Points DESC
+
+
+##########################
+##  End
+##########################
